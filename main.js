74a7049596202be80ddcab0e3d1e0b86936bcb5c
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-16 23:25:26 -0400

    Finished javascript and slight style - fully functional

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert ("Hello");
+var backgrounds = [
+"red", "white", "blue", "green", "brown", "yellow", "pink", "black", "orange"
+];
+
+// let randomColor = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+// console.log("this is my random color ", randomColor);
+
+
+
+
+    document.getElementById("color-button").addEventListener("click", function() {
+
+            randomColor = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+            console.log(randomColor);
+
+            document.body.style.background = randomColor; 
+        
+    });
